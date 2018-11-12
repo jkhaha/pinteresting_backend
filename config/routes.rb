b18ac199b08fcfdf95b7661a8cc6fcd42282b620
jkhaha <jha2@wellesley.edu>
2018-11-12 14:33:40 -0500

    pre lecture

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :images
-  resources :boards
-  resources :users
+
+
+  resources :users do
+    resources :boards do
+      resources :images
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
